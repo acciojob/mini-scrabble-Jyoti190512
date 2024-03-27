583c749297210166953a530dd876ecc31de42e58
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2024-03-27 13:15:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+document.addEventListener('DOMContentLoaded', function() {
+    const evaluatedText = document.getElementById('evaluatedText');
+    const letterCount = document.getElementById('letterCount');
+
+    evaluatedText.addEventListener('input', function() {
+        const text = this.value; // Remove leading and trailing spaces
+        const count = text.length;
+        letterCount.textContent = count;
+    });
+});
+
